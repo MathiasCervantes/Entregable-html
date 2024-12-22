f3dcdd4fad5575d10c9f86bfa961561b03f272d1
Mathias Cervantes <mcervantesportocarrer@gmail.com>
2024-12-21 20:16:11 -0500

    Entregable commit

---
+++ app.js
@@ -0,0 +1,45 @@
+document.getElementById("form_registro").addEventListener("submit", function(event){
+    event.preventDefault();
+    const nombres = document.getElementById("nombres_apellidos").value;
+    const nacionalidad = document.getElementById("nacionalidad").value;
+    const edad = document.getElementById("edad").value;
+    const Celular = document.getElementById("fonoxd").value;
+    const Correo = document.getElementById("correoxd").value;
+    const Domiciio = document.getElementById("casita").value;
+    const Tarjeta = document.getElementById("tarjeta").value;
+    
+    
+    if(edad < 18){
+    
+        alert("Debe ser mayor de 18 aÃ±os");
+        return;
+    }
+    
+    
+    
+    if(edad>100){
+        alert("Ingresar una edad valida");
+        return;
+    }
+
+    
+
+    const tablebody2xd = document.createElement('tr');
+    const tablebody = document.getElementById("tablebody");
+    tablebody2xd.innerHTML=`
+        <tr>
+            <td>${nombres}</td>
+            <td>${nacionalidad}</td>
+            <td>${edad}</td>
+            <td>${Celular}</td>
+            <td>${Correo}</td>
+            <td>${Domiciio}</td>
+            <td>${Tarjeta}</td>
+        <tr>
+    `;
+
+    tablebody.appendChild(tablebody2xd);
+    document.getElementById("tablas_datos").style.display="";
+    alert("Gracias por su compra");
+
+});
